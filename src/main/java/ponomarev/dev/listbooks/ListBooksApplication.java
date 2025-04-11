a53bf86c0c21ff80a9ecf56b51b3e671a932b36d
Maxim Ponomarev <ponomarevmv08@gmail.com>
2025-04-11 13:00:57 +0300

    init commit

---
+++ src/main/java/ponomarev/dev/listbooks/ListBooksApplication.java
@@ -0,0 +1,13 @@
+package ponomarev.dev.listbooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListBooksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ListBooksApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ponomarev/dev/listbooks/ListBooksApplicationTests.java
@@ -0,0 +1,13 @@
+package ponomarev.dev.listbooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListBooksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
